62031fe94e31b441f43279452f76b19a62ab9b45
namddph17471 <namddph17471@fpt.edu.vn>
2022-06-29 10:38:54 +0700

    getStaticProps whit data

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,11 +1,31 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next'
 import React from 'react'
 
-type Props = {}
-
-const ProductPage = (props: Props) => {
-  return (
-    <h1>ProductPage</h1>
-  )
+type getStaticProps = {
+  products:any[]
 }
 
+const ProductPage = ({products}: getStaticProps) => {
+  if (!products) return null
+  return (
+    <div>
+      {products.map(item =>(
+        <div key={item.id} >{item.name}</div>
+      ))}
+    </div>
+  )
+}
+export const getStaticProps:GetStaticProps<getStaticProps>  = async (context : GetStaticPropsContext) =>{
+  const data = await(await fetch(`http://localhost:3001/products`)).json()
+  if (!data) {
+    return {
+      notFound:true
+    }
+  }
+  return {
+    props:{
+      products:data
+    }
+  }
+}
 export default ProductPage
